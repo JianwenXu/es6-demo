f485cda4c4b7258e7acff72ce673f98bf856ca7d
xujianwen <2190833441@qq.com>
2017-10-18 14:48:13 +0800

    update array chapter

--- src/Array/array1.js
+++ src/Array/array1.js
@@ -1,11 +1,40 @@
 // 扩展运算符(...), 将一个数组转化为用逗号分隔的参数序列
+
 console.log(...[1, 2, 3]); // 1 2 3
 console.log(1, ...[2, 3, 4], 5); // 1 2 3 4 5
+
 // 该运算符主要用于函数调用
 function push(array, ...items) {
   array.push(...items);
 }
+
 var arr1 = [1, 2, 3];
-console.log(' arr1 : ', arr1);
-push(arr1, [4, 5, 6, 7]);
-console.log(' arr1 (after push) : ', arr1);
+console.log(arr1); // [1, 2, 3]
+push(arr1, ...[4, 5, 6, 7]);
+console.log(arr1); // [1, 2, 3, 4, 5, 6, 7]
+
+function add(x, y) {
+  return x + y;
+}
+
+const numbers = [4, 38];
+console.log(add(...numbers)); // 42
+
+// 扩展运算符可以与正常的函数参数结合使用，非常灵活
+function f(v, w, x, y, z) {
+  // do stuff
+}
+
+const args = [0, 1];
+
+f(-1, ...args, 2, ...[3]);
+
+// 扩展运算符后面还可以放置表达式
+let x = 0;
+const arr = [
+  ...(x > 0 ? ['a'] : []),
+  'b'
+];
+
+//如果扩展运算符后面是一个空数组，则不会产生任何效果
+console.log([...[], 1]); // [1]
--- src/Array/array2.js
+++ src/Array/array2.js
@@ -1,2 +1,57 @@
-const maxValue = Math.max(...[14, 3, 77]);
-console.log(' maxValue : ', maxValue);
+// 替代数组的apply方法
+
+// 由于扩展运算符可以展开数组，所以不再需要apply方法，将数组转为函数的参数了。
+
+let args = [0, 1, 2];
+
+// ES 5
+function f1(x, y, z) {
+  console.log('ES 5 : ', x, y, z); // ES 5: 0, 1, 2
+}
+
+f1.apply(null, args);
+
+// ES 6
+function f2(x, y, z) {
+  console.log('ES 6 : ', x, y, z);
+}
+
+f2(...args); // ES 6: 0, 1, 2
+
+// 下面是扩展运算符取代apply方法的一个实际的例子，应用Math.max方法，简化求出一个数组最大元素的写法。
+
+let arr = [14, 3, 77];
+
+// ES5
+console.log(Math.max.apply(null, arr)); // 77
+
+// ES6
+console.log(Math.max(...arr)); // 77
+
+// 另一个例子是通过push函数，将一个数组添加到另一个数组的尾部
+
+let arr1 = [0, 1, 2];
+let arr2 = [3, 4, 5];
+let arr3 = [6, 7, 8];
+
+// ES 5
+Array.prototype.push.apply(arr1, arr2);
+
+console.log(arr1); // [0, 1, 2, 3, 4, 5]
+
+// ES 6
+arr3.push(...arr2);
+
+console.log(arr3); // [6, 7, 8, 3, 4, 5]
+
+// 下面是另外一个例子。
+
+// ES5
+let date1 = new (Date.bind.apply(Date, [null, 2015, 1, 1]));
+
+console.log(date1); // Sun Feb 01 2015 00:00:00 GMT+0800 (中国标准时间)
+
+// ES6
+let date2 = new Date(...[2015, 1, 1]);
+
+console.log(date2); // Sun Feb 01 2015 00:00:00 GMT+0800 (中国标准时间)
